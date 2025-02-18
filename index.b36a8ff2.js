b4825857ae749c205159e61ba35170b0c32df52b
Artem Vasylenko <irbiskronos@gmail.com>
2025-02-18 15:11:33 +0200

    make build

---
+++ index.b36a8ff2.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,spaceBetween:16,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:2},1040:{slidesPerView:3}}});
+//# sourceMappingURL=index.b36a8ff2.js.map
